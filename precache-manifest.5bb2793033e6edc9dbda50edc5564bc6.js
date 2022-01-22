775a2d13480069c7015a5c047e03966c4c4f6f36
alexandrexpj <alexandrexpjr@hotmail.com>
2022-01-22 17:34:43 -0300

    Updates

---
+++ precache-manifest.5bb2793033e6edc9dbda50edc5564bc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf7de42d8ccdc49709b726144a7d96c",
+    "url": "/trybewars/index.html"
+  },
+  {
+    "revision": "dc11d508ba28f78ebddc",
+    "url": "/trybewars/static/css/main.3cc4c252.chunk.css"
+  },
+  {
+    "revision": "92b84c6eb3ce52a2975b",
+    "url": "/trybewars/static/js/2.93e76f1b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/trybewars/static/js/2.93e76f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc11d508ba28f78ebddc",
+    "url": "/trybewars/static/js/main.2c62a692.chunk.js"
+  },
+  {
+    "revision": "47da68aae254ca4e7498",
+    "url": "/trybewars/static/js/runtime-main.d299b53c.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/trybewars/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
